55a1cf4165ebf8e958cf25565286cfe30447fa13
Dznan <tsenantung@yahoo.com>
2019-06-24 21:00:38 +0800

    Update plot.py

--- mag/plot.py
+++ mag/plot.py
@@ -1,4 +1,5 @@
 from .cell import Cell
+from mpl_toolkits.mplot3d import Axes3D 
 from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 
@@ -9,5 +10,7 @@ def plot_cell(cells):
     for cell in cells:
         cir = Circle(xy=cell.position, radius=cell.radius, fill=None)
         ax.add_patch(cir)
+        for particle in cells.particles:
+            ax.quiver(particle.position[0], particle.position[1], particle.m[0], particle.m[1], length=0.1)
     ax.set_aspect(1)
     plt.show()
