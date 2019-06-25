fbae8634d28744c4bd36f6b3f51f9b731093f394
Dznan <tsenantung@yahoo.com>
2019-06-25 10:51:26 +0800

    Update plot.py

--- mag/plot.py
+++ mag/plot.py
@@ -11,6 +11,6 @@ def plot_cell(cells):
         cir = Circle(xy=cell.position, radius=cell.radius, fill=None)
         ax.add_patch(cir)
         for particle in cells.particles:
-            ax.quiver(particle.position[0], particle.position[1], particle.m[0], particle.m[1], length=0.1)
+            ax.quiver(particle.position[0], particle.position[1], particle.m[0], particle.m[1])
     ax.set_aspect(1)
     plt.show()
