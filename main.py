567483800e3c15482e4c707c70f70801eceb1dcf
Dznan <tsenantung@yahoo.com>
2019-06-25 15:34:50 +0800

    Minor fix

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import numpy as np
 import mag
 
 
-cells = [mag.Cell(np.random.randn(2)) for _ in range(4)]
+cells = [mag.Cell(np.random.randn(2) / 10, radius=0.01) for _ in range(4)]
 mag.plot_cell(cells)
