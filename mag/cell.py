c3ecdcb6f12868df61dc8f28a6f86b39dd80b21a
Draymond-666 <dnbs@vip.qq.com>
2019-06-25 11:38:29 +0800

    add direction vector

--- mag/cell.py
+++ mag/cell.py
@@ -3,10 +3,14 @@ import numpy as np
 
 
 class Cell:
-    def __init__(self, position=None, num_of_particle=2, radius=0.1):
+    # TODO: Ms的初始化
+    def __init__(self, position=None, Ms=1, radius=0.1):
         self.position = position
-        self.num_of_particle = num_of_particle
-        self.particles = [Particle(position) for i in range(num_of_particle)]
+        self.num_of_particle = 2
+        zero_vec = np.zeros(3)
+        m = np.random.randn(3)
+        m = m / np.linalg.norm(m)
+        self.particles = [Particle(position, m * Ms), Particle(position, -m * Ms)]
         self.radius = radius
         self.volume = 4 / 3 * np.pi ** 3
 
